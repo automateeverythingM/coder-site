7b082b5e6834ca3537377312733ac3b8f67ef332
automateeverythingM <markospavicbg@gmail.com>
2020-11-13 21:13:54 +0100

    hero

--- src/components/Hero/HeroPractice.js
+++ src/components/Hero/HeroPractice.js
@@ -3,19 +3,21 @@ import { Button, Jumbotron } from "react-bootstrap";
 import classes from "./herostyle.module.css";
 
 export default function HeroPractice() {
-  return (
-    <Jumbotron fluid className={`bg-dark text-white ${classes.ideaLight}`}>
-      <div className="container mx-auto text-center">
-        <div></div>
-        <div>
-          <h2 className="display-3 text-dark">Lights on your ideas</h2>
-          <h2>No company takes money from you</h2>
-          <h3>Build your team and freelance</h3>
-          <Button size="lg" variant="secondary">
-            Create Team
-          </Button>
-        </div>
-      </div>
-    </Jumbotron>
-  );
+    return (
+        <Jumbotron fluid className={`bg-dark text-white ${classes.ideaLight}`}>
+            <div className="container mx-auto text-center">
+                <div></div>
+                <div>
+                    <h2 className="display-3 text-dark">
+                        Lights on your ideas
+                    </h2>
+                    <h2>Share yours and work on others ideas</h2>
+                    <h3>Build your team and freelance</h3>
+                    <Button size="lg" variant="secondary">
+                        Create Team
+                    </Button>
+                </div>
+            </div>
+        </Jumbotron>
+    );
 }
